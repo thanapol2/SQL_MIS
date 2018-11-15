0043294efb093e68a649a10c2f7b5ee8d68d3840
Tong_RulE76 <thanapol.phu@gmail.com>
2018-11-15 20:53:39 +0700

    update module1
    update module1

--- ETRANS/report1.sql
+++ ETRANS/report1.sql
@@ -5,43 +5,215 @@ SELECT
     TO_CHAR(trunc(add_months(d.created_date, 6516), 'yyyy'), 'dd-MON-YYYY') AS start_date,
     TO_CHAR(trunc(add_months(d.created_date, 6528), 'yyyy') - 1, 'dd-MON-YYYY') AS end_date,
     TO_CHAR(d.created_date, 'Q') AS quater_id,
-    '????????? '
+    'ไตรมาสที่ '
     || TO_CHAR(d.created_date, 'Q') AS quater_short_name,
-    '?? '
+    'ปี '
     || TO_CHAR(TO_CHAR(d.created_date, 'yyyy') + 543)
-    || ' ????????? '
+    || ' ไตรมาสที่ '
     || TO_CHAR(d.created_date, 'Q') AS quater_long_name,
-    TO_CHAR(d.created_date, 'MM') as month_id,
-    TO_CHAR(d.created_date, 'MON') as month_short_name,
-    TO_CHAR(d.created_date, 'MONTH') as month_long_name
+    TO_CHAR(d.created_date, 'MM') AS month_id,
+    TO_CHAR(d.created_date, 'MON') AS month_short_name,
+    TO_CHAR(d.created_date, 'MONTH') AS month_long_name
 FROM
     (
         SELECT
             TO_DATE('01/01/2018', 'dd/mm/yyyy') created_date
         FROM
             dual
-    )d;
+    ) d;
 --------------------------------
+-- all list
+
 SELECT
-    tr.created_date,
-    TO_CHAR(tr.created_date, 'yyyy') + 543 AS caldr_year,
-    TO_CHAR(trunc(add_months(tr.created_date, 6516), 'yyyy'), 'dd-MON-YYYY') AS start_date,
-    TO_CHAR(trunc(add_months(tr.created_date, 6528), 'yyyy') - 1, 'dd-MON-YYYY') AS end_date,
-    TO_CHAR(tr.created_date, 'Q') AS quater_id,
-    '????????? '
-    || TO_CHAR(tr.created_date, 'Q') AS quater_short_name,
-    '?? '
-    || TO_CHAR(TO_CHAR(tr.created_date, 'yyyy') + 543)
-       || ' ????????? '
-          || TO_CHAR(tr.created_date, 'Q') AS quater_long_name,
-    tr.dept_id,
-    tr.off_id,
-    'none' AS off_name,
-    'none' AS status_cd,
-    tr.status,
-    'A' AS reqt_cnt_status
---    count(tr.status) as reqt_cnt,
---    count(tr.status)*100 as reqt_pert
+    de.month_year,
+    de.dept_id,
+    md.dept_name,
+    de.off_id,
+    'none' off_name,
+    'none' status_code,
+    de.status   AS status_desc,
+    'N' reqt_cnt_status,
+    reqt_cnt,
+    reqt_cnt * 100 / reqt_all reqt_pert
 FROM
-    token_request tr
---group by tr.dept_id,tr.off_id,tr.status
+    (
+        SELECT
+            month_year,
+            tr.dept_id,
+            tr.off_id,
+            tr.status,
+            COUNT(status) reqt_cnt
+        FROM
+            (
+                SELECT
+                    trunc(created_date, 'mon') month_year,
+                    dept_id,
+                    off_id,
+                    status
+                FROM
+                    token_request
+--                    where  trunc(created_date, 'mon') = 
+            ) tr
+        GROUP BY
+            month_year,
+            dept_id,
+            off_id,
+            status
+    ) de
+    INNER JOIN (
+        SELECT
+            trunc(created_date, 'mon') month_year,
+            dept_id,
+            off_id,
+            COUNT(*) reqt_all
+        FROM
+            token_request
+        GROUP BY
+            trunc(created_date, 'mon'),
+            dept_id,
+            off_id
+    ) al ON de.month_year = al.month_year
+            AND de.dept_id = al.dept_id
+            AND de.off_id = al.off_id
+    INNER JOIN mst_dept md ON de.dept_id = md.dept_id;
+
+--  sum year
+SELECT
+    de.month_year,
+    de.dept_id,
+    md.dept_name,
+    de.off_id || 99 off_id,
+    'รวม'||'none' off_name,
+    'none' status_code,
+    de.status   AS status_desc,
+    'N' reqt_cnt_status,
+    reqt_cnt,
+    reqt_cnt * 100 / reqt_all reqt_pert
+FROM
+    (
+        SELECT
+            month_year,
+            tr.dept_id,
+            tr.off_id,
+            tr.status,
+            COUNT(status) reqt_cnt
+        FROM
+            (
+                SELECT
+                    trunc(created_date, 'year') month_year,
+                    dept_id,
+                    off_id,
+                    status
+                FROM
+                    token_request
+            ) tr
+        GROUP BY
+            month_year,
+            dept_id,
+            off_id,
+            status
+    ) de
+    INNER JOIN (
+        SELECT
+            trunc(created_date, 'year') month_year,
+            dept_id,
+            off_id,
+            COUNT(*) reqt_all
+        FROM
+            token_request
+        GROUP BY
+            trunc(created_date, 'year'),
+            dept_id,
+            off_id
+    ) al ON de.month_year = al.month_year
+            AND de.dept_id = al.dept_id
+            AND de.off_id = al.off_id
+    INNER JOIN mst_dept md ON de.dept_id = md.dept_id;
+-- depart sum office 999999
+SELECT
+    de.month_year,
+    de.dept_id,
+    md.dept_name,
+    '999999' off_id,
+    'รวมสรรพสามิตพื้นที่ ภาคที่ '|| de.dept_id off_name,
+    'none' status_code,
+    de.status   AS status_desc,
+    'N' reqt_cnt_status,
+    reqt_cnt,
+    reqt_cnt * 100 / reqt_all reqt_pert
+FROM
+    (
+        SELECT
+            month_year,
+            tr.dept_id,
+            tr.status,
+            COUNT(status) reqt_cnt
+        FROM
+            (
+                SELECT
+                    trunc(created_date, 'mon') month_year,
+                    dept_id,
+                    status
+                FROM
+                    token_request
+            ) tr
+        GROUP BY
+            month_year,
+            dept_id,
+            status
+    ) de
+    INNER JOIN (
+        SELECT
+            trunc(created_date, 'mon') month_year,
+            dept_id,
+            COUNT(*) reqt_all
+        FROM
+            token_request
+        GROUP BY
+            trunc(created_date, 'mon'),
+            dept_id
+    ) al ON de.month_year = al.month_year
+            AND de.dept_id = al.dept_id
+    INNER JOIN mst_dept md ON de.dept_id = md.dept_id;
+    
+-- sum all
+SELECT
+    de.month_year,
+    '99' dept_id,
+    'ทั่วประเทศ' dept_name,
+    '999999' off_id,
+    'ทั่วประเทศ' off_name,
+    'none' status_code,
+    de.status   AS status_desc,
+    'N' reqt_cnt_status,
+    reqt_cnt,
+    reqt_cnt * 100 / reqt_all reqt_pert
+FROM
+    (
+        SELECT
+            month_year,
+            tr.status,
+            COUNT(status) reqt_cnt
+        FROM
+            (
+                SELECT
+                    trunc(created_date, 'mon') month_year,
+                    status
+                FROM
+                    token_request
+            ) tr
+        GROUP BY
+            month_year,
+            status
+    ) de
+    INNER JOIN (
+        SELECT
+            trunc(created_date, 'mon') month_year,
+            COUNT(*) reqt_all
+        FROM
+            token_request
+        GROUP BY
+            trunc(created_date, 'mon')
+    ) al ON de.month_year = al.month_year;
+    
+-- Type A
